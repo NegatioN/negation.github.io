02ce5c436d2c3e9d229dc74c45f6e657f6b915d5
NegatioN <4037769+NegatioN@users.noreply.github.com>
2025-09-03 19:47:46 +0200

    Add line breaks in html to quotes

--- _favorites/preprocessing/preprocess_quotes.py
+++ _favorites/preprocessing/preprocess_quotes.py
@@ -5,11 +5,14 @@ def create_id(author, text):
     h = hashlib.sha256(text.encode('utf-8'))
     return f'{author.lower().replace(" ", "_")}-{h.hexdigest()}'
 
+def wrap_newline_in_html(text):
+    return text.replace('\\n', '<br />') # Basic replacement for newlines in HTML. Might need to be better
+
 if __name__ == "__main__":
     with open('quotes_arranged', 'r') as csvfile:
-        reader = csv.reader(csvfile, quotechar='"', delimiter=',', escapechar='\\')
+        reader = csv.reader(csvfile, quotechar='"', delimiter=',')
         headers = next(reader, None)
-        output = [{'text': row[0], 'author': row[1], 'origin': row[2], 'origin_url': row[3], 'date': row[4], 'type': row[5]} for i, row in enumerate(reader)]
+        output = [{'text': wrap_newline_in_html(row[0]), 'author': row[1], 'origin': row[2], 'origin_url': row[3], 'date': row[4], 'type': row[5]} for i, row in enumerate(reader)]
         for item in output:
             item_id = create_id(item['author'], item['text'])
             fname = Path('..', f'{item_id}.md')
